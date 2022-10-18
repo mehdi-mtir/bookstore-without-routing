7f1e0e8f39f4f76daa0ab86c90e17b2d1af5762d
mehdi-mtir <mehdi.mtir@gmail.com>
2022-10-18 08:43:41 +0100

    Demo Component + Binding

---
+++ src/app/first/first.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+import { UrlHandlingStrategy } from "@angular/router";
+
+@Component({
+  selector : 'app-first',
+  templateUrl : './first.component.html'
+})
+export class FirstComponent{
+  title = 'Les types de binding : nouvelle version';
+
+  url2 = 'https://www.svgrepo.com/show/327335/logo-angular.svg';
+  url1 = 'https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg'
+  url = this.url1;
+
+
+  sayHello = (val : number)=>{
+    return 'Hello from function ' + val;
+  }
+
+  changeLogo = ()=>{
+    if(this.url === this.url1)
+      this.url = this.url2;
+    else
+      this.url = this.url1;
+  }
+
+}
